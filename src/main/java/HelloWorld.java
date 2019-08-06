5a72206420f880f0084bd8d2ca5814db24a961cd
JingDuoCI <53248841+JingDuoCI@users.noreply.github.com>
2019-08-06 20:33:57 +0800

    add time

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,13 @@
 
 public class HelloWorld {
     public static void main(String args[]) {
-        System.out.println("This to test java with maven");
+        try {
+            while (true) {
+                System.out.println("This to test java with maven: " + new java.util.Date().toString());
+                Thread.sleep(3000);
+            }
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
     }
 }
